b4d0ad555f203e2b80e556d7cf13de0753b95a05
MaryRumyantzeva <rumyantzevamary@yandex.ru>
2025-04-07 09:18:42 +0300

    bes testov

--- app/lab1/tests/test_posts.py
+++ app/lab1/tests/test_posts.py
@@ -1,8 +1,8 @@
 import sys
 import os
 
-# sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-# # sys.path.append('..')
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+#sys.path.append('..')
 
 import pytest
 from lab1.app import app, posts_list
