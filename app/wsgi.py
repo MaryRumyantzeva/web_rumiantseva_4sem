0a9cc9a3aecbf5d0486452292e37e5ea942f42f5
MaryRumyantzeva <rumyantzevamary@yandex.ru>
2025-06-20 06:42:30 +0300

    kp itog

--- app/wsgi.py
+++ app/wsgi.py
@@ -1,4 +1,14 @@
-from app import app
+import sys
+import os
 
-if __name__ == "__main__":
-    app.run()
+# Абсолютный путь до папки, где лежит твоя app.py
+project_home = '/home/rumiantseva/web_rumiantseva_4sem'
+
+# Добавляем в sys.path, если не добавлено
+if project_home not in sys.path:
+    sys.path.insert(0, project_home)
+
+# Теперь можно импортировать
+from app.app import app  # путь до DispatcherMiddleware объекта
+
+# Если запускается как main — не нужно, PythonAnywhere сам запустит
