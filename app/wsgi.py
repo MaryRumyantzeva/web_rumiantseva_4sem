7f93b6e200eac1b743124c6f05c4cb403cb665d0
MaryRumyantzeva <rumyantzevamary@yandex.ru>
2025-06-20 22:33:19 +0300

    exaam

--- app/wsgi.py
+++ app/wsgi.py
@@ -2,7 +2,7 @@ import sys
 import os
 
 # путь к корневой папке проекта
-project_path = '/home/rumiantseva/web_rumiantseva_4sem'
+project_path = r'C:\Users\rumya\OneDrive\Рабочий стол\вуз\4сем\веб\application_dispatching_example'
 if project_path not in sys.path:
     sys.path.insert(0, project_path)
 
